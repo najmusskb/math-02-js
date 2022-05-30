5504984cda564d1a19f3f07150c2977efe674c8b
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-30 22:58:12 +0600

    first commit

---
+++ shor-thand.js
@@ -0,0 +1,37 @@
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var sub = price2 - price1;
+var mul = price1 * price2;
+var div = price1 / price2;
+console.log(sum, sub, mul, div);
+var newPrise = price1 + 25;
+console.log(newPrise);
+// short hand java script code ...nicer eikhane e sob jaygay ek ek kore komteche 
+var love = 100;
+love = love - 1;
+love -= 1;
+love--;
+console.log(love);
+
+// amra jodii + korte cai tahole 
+
+var love = 100;
+love = love + 1;
+love += 1;
+love++;
+console.log(love);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
